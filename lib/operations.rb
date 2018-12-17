cc8d8c074310bc291f1f9e83b058339dcc6559d5
Andrew Kim <akim618@gmail.com>
2018-12-17 20:12:22 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,27 @@
 def unsafe?(speed)
+	if speed >= 41 && speed <=59
+		return false
+
+	elsif speed > 60
+		return true
+
+	elsif speed < 40
+		return true
 
 end
-
-
+end
 
 def not_safe?(speed)
-	
+	if speed >= 41 && speed <=59
+		return false
+
+	elsif speed > 60
+		return true
+
+	elsif speed < 40
+		return true
+
+		speed >= 41 && speed <=59 ? true : false
+
 end
-	
-
-
+end
