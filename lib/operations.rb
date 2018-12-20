1ce25c6ffeb34b3714bc74d457e2517e52b99d7e
zaheer <m.abbaszaheer@gmail.com>
2018-12-20 02:40:05 -0500

    second commit

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,15 @@
 def unsafe?(speed)
-	 	if  speed >60 || speed <40
-       	true
-   		else
-   		false
-	 	end
+	if  speed >60 || speed <40
+     true
+     else
+   	false
+	 end
 end
 
 
 def not_safe?(speed)
 
-(speed>60 || speed <40 )? true: false
+ speed.between?(40,60)?false:true
 
 end
 	
